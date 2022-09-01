160b1b2172e02d4653709cdba733cf92f3158183
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-01 16:09:56 +0200

    Update Train.py

--- train_tools/Train.py
+++ train_tools/Train.py
@@ -103,7 +103,7 @@ def train(CONFIG_PATH, CONFIG, train_loader_adversarial, val_loader_adversarial,
     train_loader_len = int(CONFIG["TRAIN_DATASET_SIZE"] / CONFIG["TRAIN_BATCH_SIZE"])
     val_loader_len = int(CONFIG["VAL_DATASET_SIZE"] / CONFIG["TRAIN_BATCH_SIZE"])
 
-    print(os.environ['MODE'], os.environ['Executor_Finished_Train'], os.environ['Executor_Finished_Val'])
+    print("ok", os.environ['MODE'], os.environ['Executor_Finished_Train'], os.environ['Executor_Finished_Val'])
     for e in range(CONFIG["EPOCHS"]):
         model = model.train()
 
