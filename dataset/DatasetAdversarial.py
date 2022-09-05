b8f5a65e033c9139f86f6aba0400c183b1446d06
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-05 10:44:32 +0200

    Update DatasetAdversarial.py

--- dataset/DatasetAdversarial.py
+++ dataset/DatasetAdversarial.py
@@ -3,15 +3,12 @@ import torch
 import time
 import sys
 
-from util.Comunication import Comunication
-
 class DatasetAdversarial:    
     def __init__(self, con_conf_path, data_queue_path, slice_, mode_):
         self.con_conf_path = con_conf_path
         self.data_queue_path = data_queue_path
         self.slice_ = slice_
         self.mode_ = mode_
-        self.comunication = Comunication()
         
     def __len__(self):
         return sys.maxsize
@@ -22,8 +19,7 @@ class DatasetAdversarial:
         
         path_a = int(idx / self.slice_)
         path_b = idx % self.slice_
-        
-        count_no_data = 0
+
         image_path = self.data_queue_path + "image_" + str(path_a) + "_" + str(path_b) + "_.pt"
         label_path = self.data_queue_path + "label_" + str(path_a) + "_" + str(path_b) + "_.pt"
 
@@ -42,19 +38,6 @@ class DatasetAdversarial:
                 except Exception as e:
                     return [remove_queue]
             else:
-                count_no_data += 1
-                if(count_no_data > 1 and count_no_data % 2000 == 0):
-                    print("waiting for data sice:" + str(0.01 * count_no_data)[:5] + "(s)...")
-                
-                if(count_no_data > 1 and count_no_data % 200 == 0):                    
-                    con_conf = self.comunication.readConf()
-                    
-                    if(
-                        (self.mode_ == "train" and con_conf['Executor_Finished_Train'] == "True") or
-                        (self.mode_ == "val" and con_conf['Executor_Finished_Val'] == "True")
-                    ):
-                        return []
-                
-                time.sleep(0.01)
+                return []
                 
         return [image_, label_, remove_queue]
