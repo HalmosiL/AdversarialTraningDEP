a5708348096357ca87cbd44fde6529adeceb943d
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-06 10:15:59 +0200

    Update Train.py

--- train_tools/Train.py
+++ train_tools/Train.py
@@ -129,8 +129,6 @@ def train(CONFIG_PATH, CONFIG, train_loader_adversarial_, val_loader_adversarial
         
         while(batch_id == 0 or len(data)):
             data = next(train_loader_adversarial_iter)
-            print(type(data[0].item()))
-            print(data[0].item())
             
             if(len(data) == 3):
                 image = data[0][0].to(CONFIG["DEVICE"][0])
