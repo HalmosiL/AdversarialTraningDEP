890a4267b38532f48f0ef8421b472be5ad902e96
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-06 08:46:18 +0200

    Update Sampler.py

--- util/Sampler.py
+++ util/Sampler.py
@@ -4,17 +4,23 @@ import sys
 class Iterable:
   def __init__(self):
     self.index_ =  0
+    self.backQueueidx_ = []
     
   def __getitem__(self, idx):
-    index_ = self.index_
-    self.index_ += 1
+    if(len(self.backQueueidx_) == 0)
+      index_ = self.index_
+      self.index_ += 1
+    else
+      index_ = self.backQueueidx_[0]
+      self.backQueueidx_.pop(0)
+      
     return index_
 
   def __len__(self):
       return sys.maxint
     
-  def __back__(self):
-    self.index_ += 1
+  def __back__(self, idx_):
+    self.backQueue.append(idx_)
 
 class FileSampler(torch.utils.data.Sampler):
   def __init__(self):
