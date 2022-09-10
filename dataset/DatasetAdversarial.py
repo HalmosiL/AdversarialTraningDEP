fe9f4c2f8a50156c0a58ea4361c46f4829ee5c3b
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-10 15:53:18 +0200

    Update DatasetAdversarial.py

--- dataset/DatasetAdversarial.py
+++ dataset/DatasetAdversarial.py
@@ -20,20 +20,32 @@ class DatasetAdversarial:
         path_a = int(idx / self.slice_)
         path_b = idx % self.slice_
 
-        image_path = self.data_queue_path + "image_" + str(path_a) + "_" + str(path_b) + "_.pt"
-        label_path = self.data_queue_path + "label_" + str(path_a) + "_" + str(path_b) + "_.pt"
+        image_normal_path = self.data_queue_path + "image_normal_" + str(path_a) + "_" + str(path_b) + "_.pt"
+        label_normal_path = self.data_queue_path + "label_normal" + str(path_a) + "_" + str(path_b) + "_.pt"
+        image_adversarial_path = self.data_queue_path + "image_adversarial" + str(path_a) + "_" + str(path_b) + "_.pt"
+        label_adversarial_path = self.data_queue_path + "label_adversarial" + str(path_a) + "_" + str(path_b) + "_.pt"
 
         if(
-            os.path.exists(image_path) and
-            os.path.exists(label_path)
+            os.path.exists(image_normal_path) and
+            os.path.exists(label_normal_path) and
+            os.path.exists(image_adversarial_path) and
+            os.path.exists(label_adversarial_path)
         ):
             try:
                 count_no_data = 0
-                image_ = torch.load(image_path).clone()
-                label_ = torch.load(label_path).clone()
-                return [image_.reshape(1, *image_.shape), label_.reshape(1, *label_.shape), [image_path, label_path]]
+                image_normal = torch.load(image_normal_path).clone()
+                label_normal = torch.load(label_normal_path).clone()
+                image_adversaria = torch.load(image_adversarial_path).clone()
+                label_adversaria = torch.load(label_adversarial_path).clone()
+                return [
+                    image_normal.reshape(1, *image_.shape),
+                    label_normal.reshape(1, *label_.shape),
+                    image_adversaria.reshape(1, *image_.shape),
+                    label_adversaria.reshape(1, *label_.shape),
+                    [image_normal_path, label_normal_path, image_adversarial_path, label_adversarial_path]
+                ]
             except Exception as e:
                 print("wrong")
-                return [[image_path, label_path]]
+                return [[image_normal_path, label_normal_path, image_adversarial_path, label_adversarial_path]]
                 
         return []
