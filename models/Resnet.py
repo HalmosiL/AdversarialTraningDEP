fccb106ad16dcdc4d323315f1becfbef2861f8fd
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-12 01:12:08 +0200

    Update Resnet.py

--- models/Resnet.py
+++ models/Resnet.py
@@ -200,7 +200,7 @@ def resnet50(pretrained=False, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-    if False:
+    if pretrained:
         #model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
         model.load_state_dict(torch.load(model_paths["resnet50"]), strict=False)
     return model
