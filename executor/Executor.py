7679122c390421f2e875fe9f92150d296700a3aa
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-12 00:57:03 +0200

    Update Executor.py

--- executor/Executor.py
+++ executor/Executor.py
@@ -79,6 +79,7 @@ class Executor:
         mean = [0.485, 0.456, 0.406]
         mean = [item * value_scale for item in mean]
         std = [0.229, 0.224, 0.225]
+        std = [item * value_scale for item in std]
 
         train_transform = transform.Compose([
             transform.RandScale([args_dataset["scale_min"], args_dataset["scale_max"]]),
