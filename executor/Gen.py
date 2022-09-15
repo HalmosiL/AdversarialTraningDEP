e59d56be89ae3c23374584fec7caa5f13275d25f
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-15 13:52:44 +0200

    Update Gen.py

--- executor/Gen.py
+++ executor/Gen.py
@@ -5,9 +5,9 @@ def run(id_, batch, device, model, attack, number_of_steps, data_queue, split, s
     print("Gen_", id_, " started..")
     if(gen):
         image = batch[0].to(device)
-        image = torch.split(image, int(len(image)/4))
-        image_normal = torch.cat((image[0], image[1], image[2]), dim=0)
-        image_adversarial = image[3]
+        image = torch.split(image, int(len(image)/3))
+        image_normal = torch.cat((image[0], image[1]), dim=0)
+        image_adversarial = image[2]
         
         image_adversarial = model_immer_attack_auto_loss(
             image=image_adversarial,
@@ -18,18 +18,18 @@ def run(id_, batch, device, model, attack, number_of_steps, data_queue, split, s
         )
         
         label = batch[1]
-        label = torch.split(label, int(len(label)/4))
-        label_normal = torch.cat((label[0], label[1], label[2]), dim=0)
-        label_adversarial = label[3]
+        label = torch.split(label, int(len(label)/3))
+        label_normal = torch.cat((label[0], label[1]), dim=0)
+        label_adversarial = label[2]
 
         if(split == -1 or split == 1):
             torch.save(torch.cat(image_normal.cpu().detach(), image_adversarial.cpu().detach()), data_queue + 'image_' + str(id_) + '_0_.pt')
             torch.save(torch.cat(label_normal.cpu().detach(), label_adversarial.cpu().detach()), data_queue + 'label_' + str(id_) + '_0_.pt')
         else:
-            image_normal = torch.split(image_normal, int(split_size * 3 / 4))
-            image_adversarial = torch.split(image_adversarial, int(split_size/4))
-            label_normal = torch.split(label_normal, int(split_size * 3 / 4))
-            label_adversarial = torch.split(label_adversarial, int(split_size/4))
+            image_normal = torch.split(image_normal, int(split_size * 2 / 3))
+            image_adversarial = torch.split(image_adversarial, int(split_size/3))
+            label_normal = torch.split(label_normal, int(split_size * 2 / 3))
+            label_adversarial = torch.split(label_adversarial, int(split_size/3))
 
             for i in range(len(image_normal)):
                 print("save:", data_queue + 'image_' + str(id_) + '_' + str(i) + '_.pt')
