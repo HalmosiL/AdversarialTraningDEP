e4141bd513851451d3814d8418cf3c9f4bda4f9b
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-18 17:55:49 +0200

    Update Train.py

--- train_tools/Train.py
+++ train_tools/Train.py
@@ -57,7 +57,16 @@ def train(CONFIG_PATH, CONFIG, train_loader_adversarial_, val_loader_adversarial
 
         print("Load Model.....")
         model.load_state_dict(torch.load(CONFIG["MODEL_CONTINUM_PATH"])["state_dict"])
-        optimizer = torch.optim.Adam(model.parameters(), lr=CONFIG['LEARNING_RATE'])
+        optimizer = torch.optim.SGD(
+            [{'params': model.layer0.parameters()},
+            {'params': model.layer1.parameters()},
+            {'params': model.layer2.parameters()},
+            {'params': model.layer3.parameters()},
+            {'params': model.layer4.parameters()},
+            {'params': model.ppm.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10},
+            {'params': model.cls.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10},
+            {'params': model.aux.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10}],
+            lr=CONFIG['LEARNING_RATE'], momentum=CONFIG['MOMENTUM'], weight_decay=CONFIG['WEIGHT_DECAY'])
 
         print("Load optimizer.....")
         optimizer.load_state_dict(torch.load(CONFIG["OPTIMIZER_CONTINUM_PATH"])["optimizer"])
@@ -69,12 +78,30 @@ def train(CONFIG_PATH, CONFIG, train_loader_adversarial_, val_loader_adversarial
 
         print("Load Model.....")
         model.load_state_dict(torch.load(CONFIG["MODEL_CONTINUM_PATH"])["state_dict"])
-        optimizer = torch.optim.Adam(model.parameters(), lr=CONFIG['LEARNING_RATE'])
+        optimizer = torch.optim.SGD(
+            [{'params': model.layer0.parameters()},
+            {'params': model.layer1.parameters()},
+            {'params': model.layer2.parameters()},
+            {'params': model.layer3.parameters()},
+            {'params': model.layer4.parameters()},
+            {'params': model.ppm.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10},
+            {'params': model.cls.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10},
+            {'params': model.aux.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10}],
+            lr=CONFIG['LEARNING_RATE'], momentum=CONFIG['MOMENTUM'], weight_decay=CONFIG['WEIGHT_DECAY'])
 
         print("Traning started.....")
     else:
         model = get_model(CONFIG['DEVICE_TRAIN'])
-        optimizer = torch.optim.Adam(model.parameters(), lr=CONFIG['LEARNING_RATE'])
+        optimizer = torch.optim.SGD(
+            [{'params': model.layer0.parameters()},
+            {'params': model.layer1.parameters()},
+            {'params': model.layer2.parameters()},
+            {'params': model.layer3.parameters()},
+            {'params': model.layer4.parameters()},
+            {'params': model.ppm.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10},
+            {'params': model.cls.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10},
+            {'params': model.aux.parameters(), 'lr': CONFIG['LEARNING_RATE'] * 10}],
+            lr=CONFIG['LEARNING_RATE'], momentum=CONFIG['MOMENTUM'], weight_decay=CONFIG['WEIGHT_DECAY'])
         print("Traning started.....")
     
     cache_id = 0
