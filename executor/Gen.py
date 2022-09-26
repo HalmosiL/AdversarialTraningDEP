2c8fd645ba28da96be0a8707966e74e80e06cc53
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-26 11:29:37 +0200

    Update Gen.py

--- executor/Gen.py
+++ executor/Gen.py
@@ -39,18 +39,6 @@ def run(id_, batch, device, model, attack, number_of_steps, data_queue, split, s
             
             image_adversarial = torch.split(image_adversarial, int(len(image_adversarial) / 2))
             label_adversarial = torch.split(label_adversarial, int(len(image_adversarial) / 2))
-
-            print(image_normal[0].shape)
-            print(label_normal[0].shape)
-            
-            print(image_adversarial[0].shape)
-            print(label_adversarial[0].shape)
-            
-            print(image_normal[1].shape)
-            print(label_normal[1].shape)
-            
-            print(image_adversarial[1].shape)
-            print(label_adversarial[1].shape)
             
             for i in range(len(image_normal)):
                 logging.debug("save:" + data_queue + 'image_' + str(id_) + '_' + str(i) + '_.pt')
